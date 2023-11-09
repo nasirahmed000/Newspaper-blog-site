29f42f933f81752b2c831af3a1dca98f4359eaa2
Nasir Ahmed <ahsannasirahmed@gmail.com>
2023-11-09 18:36:09 +0600

    10

--- details.php
+++ details.php
@@ -14,65 +14,7 @@ if( isset($_GET ['id']) ){
     while ($final = $s_sql -> fetch_assoc()){
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Website Description -->
-    <meta name="description" content="MyNews: Corporate Multi Purpose News Blogs Template" />
-    <meta name="author" content="Minhaz" />
-
-    <!--  Favicons / Title Bar Icon  -->
-    <link rel="icon" href="images/favicon/favicon.png" />
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="vendor/css/font-awesome.min.css">
-    <link rel="stylesheet" href="vendor/css/bootstrap.css">
-    <link rel="stylesheet" href="vendor/css/style.css">
-    <link rel="stylesheet" href="vendor/css/media.css">
-
-    <title>MyNews</title>
-  </head>
-  <body class="bg-light">
-
-    <!-- header start -->
-    <header>
-      <div class="container">
-        <!-- nav -->
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="container-fluid">
-            <!-- logo -->
-            <a class="navbar-brand logo" href="index.php"><h1 class="m-0">MyNews</h1></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- menu -->
-            <div class="collapse navbar-collapse menu" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">FAQ</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Log In</a>
-                </li>
-              </ul>
-              
-            </div>
-          </div>
-        </nav>
-        <!-- nav -->
-      </div>
-    </header>
+<?php  include "header.php"  ?>
     <!-- header end -->
 
     <!-- detail news start -->
@@ -171,63 +113,7 @@ if( isset($_GET ['id']) ){
     <!-- category end -->
 
     <!-- footer start -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3">
-            <div class="f_text">
-              <h2>MyNews</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nulla cupiditate adipisci assumenda</p>
-              <p>libero qui, nobis consequuntur maiores pariatur magni atque impedit quibusdam officiis vel</p>
-            </div>
-          </div>
-          <div class="col-lg-3">
-            <div class="f_text">
-              <h2>Useful links</h2>
-              <ul class="list-unstyled">
-                <li><a href="" class="text-dark">Privacy policy</a></li>
-                <li><a href="" class="text-dark">Terms & conditions</a></li>
-                <li><a href="" class="text-dark">Privacy policy</a></li>
-                <li><a href="" class="text-dark">Terms & conditions</a></li>
-                <li><a href="" class="text-dark">Privacy policy</a></li>
-                <li><a href="" class="text-dark">Terms & conditions</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-lg-3">
-            <div class="f_text">
-              <h2>Contact Info</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nulla cupiditate adipisci assumenda </p>
-              <p>libero qui, nobis consequuntur maiores pariatur magni atque impedit quibusdam officiis vel</p>
-            </div>
-          </div>
-          <div class="col-lg-3">
-            <div class="f_text">
-              <h2>Join Us</h2>
-              <form action="#">
-                <div class="mb-3">
-                  <label for="formGroupExampleInput" class="form-label">Your Email Address</label>
-                  <input type="text" class="form-control" id="formGroupExampleInput" >
-                </div>
-                <div class="mb-3">
-                  <button class="btn btn-dark w-100">Subscribe here</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-10">
-            <p class="text-center m-0 copy">MyNews © 2022 All rights reserved.  Lorem ipsum dolor, sit amet.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <!-- footer end -->
-
-    
+   <?php  include "footer.php"; ?>
 
     
     <!-- JS links -->
